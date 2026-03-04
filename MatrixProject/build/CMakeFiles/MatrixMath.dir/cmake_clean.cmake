18b648ab2e6c5d6a99d9001353d9144e568ac8b7
Sarwat <saadaat@Saadaats-MacBook-Air.local>
2026-03-04 13:18:12 -0600

    created MatrixProject for DLL Libraries assignment

---
+++ MatrixProject/build/CMakeFiles/MatrixMath.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MatrixMath.dir/matrix_math.cpp.o"
+  "CMakeFiles/MatrixMath.dir/matrix_math.cpp.o.d"
+  "libMatrixMath.dylib"
+  "libMatrixMath.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MatrixMath.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
